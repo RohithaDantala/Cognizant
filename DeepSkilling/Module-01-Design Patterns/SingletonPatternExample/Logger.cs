64dc60476c9d06a95105f16757b0fc6180728f0e
RohithaDantala <rohithadantala08@gmail.com>
2026-06-21 13:17:40 +0530

    Adding Exercise 1 and 2

---
+++ DeepSkilling/Module-01-Design Patterns/SingletonPatternExample/Logger.cs
@@ -0,0 +1,18 @@
+using System;
+public sealed class Logger
+{
+    private static Logger? instance;
+    private Logger(){}
+    public static Logger GetInstance()
+    {
+        if (instance == null)
+        {
+            instance = new Logger();
+        }
+        return instance;
+    }
+    public void Log(string message)
+    {
+        Console.WriteLine($"Log: {message}");
+    }
+}
---
+++ DeepSkilling/Module-01-Design Patterns/SingletonPatternExample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Logger logger1 = Logger.GetInstance();
+        Logger logger2 = Logger.GetInstance();
+
+        logger1.Log("This is message 1.");
+        logger2.Log("This is message 2.");
+        if(Object.ReferenceEquals(logger1, logger2))
+        {
+            Console.WriteLine("Only one logger instance exists.");
+        }
+       
+    }
+}
