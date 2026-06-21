fc7d70c8c324f445f9d18781ae7a0b927a48771c
RohithaDantala <rohithadantala08@gmail.com>
2026-06-21 13:30:56 +0530

    Adding Exercise 2 and 7

---
+++ DeepSkilling/Module-02-DSA/Exercise2-EcommerceSearch/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Product[] products =
+        {
+            new Product(101,"Laptop","Electronics"),
+            new Product(102,"Phone","Electronics"),
+            new Product(103,"Shoes","Fashion"),
+            new Product(104,"Watch","Accessories"),
+            new Product(105,"Bag","Fashion")
+        };
+
+        Product? linearResult = SearchService.LinearSearch(products, 104);
+
+        if (linearResult != null)
+        {
+            Console.WriteLine("Linear Search Found: " + linearResult.ProductName);
+        }
+
+        Product? binaryResult = SearchService.BinarySearch(products, 104);
+
+        if (binaryResult != null)
+        {
+            Console.WriteLine("Binary Search Found: " + binaryResult.ProductName);
+        }
+    }
+}
---
+++ DeepSkilling/Module-02-DSA/Exercise7-FinancialForecasting/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double currentValue = 10000;
+        double growthRate = 0.10;
+        int years = 5;
+
+        double futureValue =
+            FinancialForecast.PredictFutureValue(currentValue, growthRate, years);
+
+        Console.WriteLine("Future Value: " + futureValue);
+    }
+}
